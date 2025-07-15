ff69a49e4e09f81ea6863c4bdc8a7d2a849c8741
Jim Dinias <129774378+jimmydin7@users.noreply.github.com>
2025-07-15 23:59:49 +0300

    updated and made rules togglable + added image

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -30,3 +30,18 @@ document.querySelectorAll('.card-hover').forEach(card => {
         card.style.transition = 'all 0.6s ease';
         observer.observe(card);
     });
+
+    
+
+
+
+  const toggleBtn = document.getElementById('toggleRules');
+  const rulesList = document.getElementById('rulesList');
+  const toggleArrow = document.getElementById('toggleArrow');
+
+  toggleBtn.addEventListener('click', () => {
+    rulesList.classList.toggle('hidden');
+    toggleArrow.classList.toggle('rotate-90');
+  });
+
+
