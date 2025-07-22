0789a726cf7e888e6be7df1f74fd81a0f95455c3
Akaalroop Singh <spacexplorer11@proton.me>
2025-07-22 22:09:16 +0100

    Added title loading from JSON

--- tutorial/tutorial/src/routes/+page.svelte
+++ tutorial/tutorial/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import {onMount} from "svelte";
+	import {base} from "$app/paths";
+
+	let tutorialPage = 0;
+
+	let data = null;
+
+	const tutorialsNames = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+	let tutorials = [];
+
+	// Load JSON on component mount
+	onMount(async () => {
+		tutorials = await Promise.all(
+			  tutorialsNames.map(i => fetch(`${base}/${i}.json`).then(res => res.json()))
+	  );
+	});
+</script>
+<div id="content">
+<h1>Tutorial</h1>
+	<div id="tutorial">
+		{#if tutorials[tutorialPage]}
+		  <h2>{tutorials[tutorialPage].title}</h2>
+		{:else}
+		  <h2>Loading...</h2>
+		{/if}
+		<button id="tutorialButton" on:click={tutorialPage++}>Next -></button>
+	</div>
+</div>
